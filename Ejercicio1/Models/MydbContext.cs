07323e2cacb027057721debab02b91a1a02e7b6a
Your Name <you@example.com>
2025-05-08 13:15:37 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Models/MydbContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace Ejercicio1.Models;
+
+public partial class MydbContext : DbContext
+{
+    public MydbContext()
+    {
+    }
+
+    public MydbContext(DbContextOptions<MydbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Idusuarios).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.Idusuarios)
+                .HasColumnType("int(11)")
+                .HasColumnName("idusuarios");
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(45)
+                .HasColumnName("apellido");
+            entity.Property(e => e.Coreo)
+                .HasMaxLength(45)
+                .HasColumnName("coreo");
+            entity.Property(e => e.FechaRegistro)
+                .HasColumnType("datetime")
+                .HasColumnName("fecha_registro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(45)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Password)
+                .HasMaxLength(45)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Ejercicio1/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejercicio1.Models;
+
+public partial class Usuario
+{
+    public int Idusuarios { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Coreo { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public DateTime FechaRegistro { get; set; }
+}
